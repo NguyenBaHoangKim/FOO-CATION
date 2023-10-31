ac3ee509a23545ce390dfc72e0e2a14d38449132
huynnnnn <100114998+huynnnnn@users.noreply.github.com>
2023-10-31 18:13:16 +0700

    pop up

---
+++ app/src/main/java/com/example/myapplication/PopupStartFragment.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class PopupStartFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.location_detail, container, false)
+        return view
+    }
+}
