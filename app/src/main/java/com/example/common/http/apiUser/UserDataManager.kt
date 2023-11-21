bd0273dacaa0b52efda06fe0d1f07f1700268caf
NguyenBaHoangKim <baloctq@gmail.com>
2023-11-21 18:04:31 +0700

    login reques

--- app/src/main/java/com/example/common/http/apiUser/UserDataManager.kt
+++ app/src/main/java/com/example/common/http/apiUser/UserDataManager.kt
@@ -1,31 +1,38 @@
-//package com.example.common.apiUser
-//
-//import android.util.Log
-//import com.example.common.http.CustomApi
-//import com.example.model.LoginRequest
-//import com.example.model.LoginResp
-//import retrofit2.Call
-//import retrofit2.Callback
-//import retrofit2.Response
-//
-//class UserDataManager {
-//    private val usersApi: UserApi = CustomApi.client()
-//    fun login(loginInfo: LoginRequest, onSuccess: (LoginResp) -> Unit, onFailure: (error: String) -> Unit){
-//        usersApi.login(loginInfo).enqueue(object : Callback<LoginResp?> {
-//            override fun onResponse(
-//                call: Call<LoginResp?>,
-//                response: Response<LoginResp?>
-//            ) {
-//                print(response.body())
-//                val responseBody = response.body() ?: return
-//                onSuccess(responseBody)
-//            }
-//
-//            override fun onFailure(call: Call<LoginResp?>, t: Throwable) {
-//                onFailure(t.message!!)
-//                Log.d("MainActivity", "onFailure: " + t.message)
-//            }
-//        })
-//    }
-//
-//}
+package com.example.common.apiUser
+
+import android.util.Log
+import com.example.common.http.CustomApi
+import com.example.model.LoginRequest
+import com.example.model.LoginResp
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class UserDataManager {
+    private val usersApi: UserApi = CustomApi.client()
+    fun login(loginInfo: LoginRequest, onSuccess: (LoginResp) -> Unit, onFailure: (error: String) -> Unit){
+        usersApi.login(loginInfo).enqueue(object : Callback<LoginResp?> {
+            override fun onResponse(
+                call: Call<LoginResp?>,
+                response: Response<LoginResp?>
+            ) {
+                println(response.code())
+                if(response.code() == 404 || response.body() == null) {
+                    println("Dang nhap k thanh cong")
+                    onFailure("")
+                } else {
+                    println(response.body())
+                    val responseBody = response.body() ?: return
+                    onSuccess(responseBody)
+                }
+            }
+
+            override fun onFailure(call: Call<LoginResp?>, t: Throwable) {
+                println("huhu ")
+                onFailure(t.message!!)
+                Log.d("MainActivity", "onFailure: " + t.message)
+            }
+        })
+    }
+
+}
